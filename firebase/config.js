c9134502414e399f75b7ea3a17c11e2142acb7c5
Heber Josias <holmosq@gmail.com>
2020-10-03 17:55:24 -0500

    firebase demo

--- firebase/config.js
+++ firebase/config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBKbov_2U1qyvGfUdyaCNFpr17MmLnvLNs",
-    authDomain: "testcovid-7bd5d.firebaseapp.com",
-    databaseURL: "https://testcovid-7bd5d.firebaseio.com",
-    projectId: "testcovid-7bd5d",
-    storageBucket: "testcovid-7bd5d.appspot.com",
-    messagingSenderId: "319370809527",
-    appId: "1:319370809527:web:d6c478f7ba505f4b9dbd32"
+    apiKey: "AIzaSyAC_p3nHl8lnp9FzTetU_dwFc-6ZTHXiyE",
+    authDomain: "covid19-e44c8.firebaseapp.com",
+    databaseURL: "https://covid19-e44c8.firebaseio.com",
+    projectId: "covid19-e44c8",
+    storageBucket: "covid19-e44c8.appspot.com",
+    messagingSenderId: "48172663443",
+    appId: "1:48172663443:web:835cf9897149a681230366"
   };
 
   export default firebaseConfig;
